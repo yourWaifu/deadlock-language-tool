cc4ab94cb09fd2882efbc226a0ce1fd318905c00
Sleepy Flower <wuhao64@gmail.com>
2024-10-30 12:00:52 -0400

    fix postProcess code not working

--- tchinese.js
+++ tchinese.js
@@ -81,13 +81,14 @@ function createConfig(){
             onAllText();
             return translateSingle(textToTranslate, id);
         },
-        "postProcess": ({translatedText, usesPlurals, pluralKey}) => {
+        "postProcess": (data) => {
+            let {translatedText, usesPlurals, pluralKey} = data;
             if (languageCode === "zh-tw" && usesPlurals && pluralKey === null) {
                 // handle strange edge case, plurals without a variable in chinese.
                 // for some reason, it needs two plurals when there's only one plural rule.
                 // adding a plural separator and a dup should make it behave as one plural
                 // rule while using two plurals
-                translatedText = `${translatedText}#|#${translatedText}`;
+                data.translatedText = `${translatedText}#|#${translatedText}`;
             }
         },
     };
